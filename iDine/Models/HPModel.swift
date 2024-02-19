6a0d3e5f3d0e972d3f4d87011b489cd7a86b9946
agholson <87455899+agholson@users.noreply.github.com>
2024-02-19 13:21:57 -0500

    Experimented with ways to dynamically update the colors for the profile image

--- iDine/Models/HPModel.swift
+++ iDine/Models/HPModel.swift
@@ -29,23 +29,19 @@ struct Patient: Codable, Hashable, Identifiable {
     var Plan: String
     
     // Image handling
-    var mainImageName: String? // Name of the image in the bundle/assets
     var userSelectedImage: UIImage? // Dynamically selected or stored image
     
     enum CodingKeys: String, CodingKey {
         case id, name, Location, Room, Diagnosis1, Restrictions, Physician, CC, HPI, MedHx, SurgHx, SocHx, FamHx, ROS, Allergies, Medications, Vaccinations, PE, Assess, Plan
         // Add other property keys but omit `userSelectedImage` as UIImage is not directly Codable
-        case mainImageName
     }
     
     // Provide a computed property to determine the current image source
     var currentImage: Image {
         if let userImage = userSelectedImage {
             return Image(uiImage: userImage)
-        } else if let imageName = mainImageName, UIImage(named: imageName) != nil {
-            // Check if the image exists in the assets and return it if so
-            return Image(imageName)
-        } else {
+        }
+        else {
             // The old logic to generate image names can still be used here as a secondary fallback
             let generatedImageName = name.replacingOccurrences(of: " ", with: "-").lowercased() + "-thumb"
             // Check if the generated image exists in the assets
