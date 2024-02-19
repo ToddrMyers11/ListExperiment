275ede3c931eab7c7c2213ec10c5f5a7e048dfb7
agholson <87455899+agholson@users.noreply.github.com>
2024-02-19 12:54:07 -0500

    Enables clicking on the photo icon of the person to change the image.

--- iDine/Views/PatientDetail.swift
+++ iDine/Views/PatientDetail.swift
@@ -17,32 +17,35 @@ struct PatientDetail: View {
     
     var body: some View {
         VStack {
-            // MARK: - Patient Photo
-            ZStack(alignment: .bottomTrailing) {
-                patient.currentImage
-                    .resizable()
-                    .scaledToFill()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 250, height: 250) // Set the frame size as needed
-                    .clipShape(Circle()) // Clip the image to a circle
-                    .overlay(
-                        Circle().stroke(.black, lineWidth: 2) // Optional: Adds a white stroke to the circle
-                    )
                 // MARK: Take Photo Options
                 Button(action: {
                     // This will toggle the action sheet on tap
                     self.showActionSheet = true
                 }) {
-                    // Overlay the pencil/edit icon at the bottom right corner
-                    Image(systemName: "pencil.circle.fill")
-                        .font(.system(size: 20)) // Adjust the size as needed
-                        .foregroundColor(.blue) // Set the foreground color as needed
-                        .background(Color.white) // Set a background to make the icon stand out
-                        .clipShape(Circle()) // Clip the background to a circle
-                        .offset(x: 20, y: 20) // Adjust the offset to position the icon correctly
-                        .padding(10) // Add padding to ensure the icon is not touching the edges
+                    // MARK: - Patient Photo
+                    ZStack(alignment: .bottomTrailing) {
+                        patient.currentImage
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 250, height: 250) // Set the frame size as needed
+                            .clipShape(Circle()) // Clip the image to a circle
+                            .overlay(
+                                Circle().stroke(.black, lineWidth: 2) // Optional: Adds a white stroke to the circle
+                            )
+                            .foregroundColor(.black) // This ensures the symbol color is black
+
+                        // MARK: Pencil Icon
+                        // Overlay the pencil/edit icon at the bottom right corner
+                        Image(systemName: "pencil.circle.fill")
+                            .font(.system(size: 20)) // Adjust the size as needed
+                            .foregroundColor(.blue) // Set the foreground color as needed
+                            .background(Color.white) // Set a background to make the icon stand out
+                            .clipShape(Circle()) // Clip the background to a circle
+                            .offset(x: 20, y: 20) // Adjust the offset to position the icon correctly
+                            .padding(10) // Add padding to ensure the icon is not touching the edges
+                    }
                 }
-            }
+             // MARK: Actions for Photo
             .actionSheet(isPresented: $showActionSheet) {
                 ActionSheet(
                     title: Text("Update Patient Photo"),
@@ -64,7 +67,7 @@ struct PatientDetail: View {
                 // TODO: Implement Camera Option
                 // TODO: Implement Photo Library
                 // This should be your ImagePicker component that handles taking a photo or choosing from the library
-               Text("Photo option")
+                Text("Photo option")
             }
             
             // MARK: - Patient Name
